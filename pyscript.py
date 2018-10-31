cf2dc1e46eb22695fff39ee4788c99d7949c4012
Malmunea <almunea.mohammed@gmail.com>
2018-10-31 11:36:30 -0700

    -Open from file implemented -Apply RegEx to extract IP implement

--- pyscript.py
+++ pyscript.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python
-import subprocess
 
-#COMMAND = "grep -E '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' ./output.txt"  
-
-#subprocess.call(COMMAND, shell=True)
-
+import pexpect
 import sys
- 
-HOST="127.0.0.1"
-ssh = subprocess.Popen(["ssh",
-                        "%s" % HOST],
-                        stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        universal_newlines=True,
-                        bufsize=0)
- 
-# send ssh commands to stdin
-ssh.stdin.write("ls .\n")
-ssh.stdin.write("uname -a\n")
-ssh.stdin.write("uptime\n")
-ssh.stdin.close()
- 
-# fetch output
-for line in ssh.stdout:
-	print(line),
-#subprocess.call(commands, shell=True)
+import re
+
+### Open a file to mimic show ip BGP
+F = open("output.txt", "r")
+output = F.read()
+print(output)
+# output = "172.123.123.123"
+
+### RegEx
+RegEx = re.compile(r"(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)",re.M|re.I)
+matchObj = RegEx.findall(output)
+
+# print(matchObj.group())
+for x in matchObj:
+    print(x)
